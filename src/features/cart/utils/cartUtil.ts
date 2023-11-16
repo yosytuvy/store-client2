ddce50e568a7d6058e7badd748e34b62dd47cbfb
Zalman-Philip <z0533147971@gmail.com>
2023-11-16 09:13:26 +0200

    cart

--- src/features/cart/utils/cartUtil.ts
+++ src/features/cart/utils/cartUtil.ts
@@ -1,8 +1,42 @@
-import { CartItemInterface } from "../interfaces/cartItemInterface";
+import { ProductInCartInterface } from "../interfaces/cartItemInterface";
 
-export const addProductToCart = () => {}; 
-export const addQuantityOfProduct = () => {};
-export const subQuantityOfProduct = () => {};
-export const removeProductFromCart = () => {};
-export const createCart = (cartItem: CartItemInterface) => {};
- 
+export const saveCartToLocalStorage = (cart: ProductInCartInterface[]) => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
+
+export const getCartFromLocalStorage = () => {
+  const cart = localStorage.getItem("cart");
+  if (!cart) return null;
+  return JSON.parse(cart) as ProductInCartInterface[];
+};
+
+export const addProductToCart = (product: ProductInCartInterface) => {
+  const cart = getCartFromLocalStorage();
+  if (!cart) return saveCartToLocalStorage([product]);
+  cart.push(product);
+  saveCartToLocalStorage(cart);
+};
+
+export const addQuantityOfProduct = (productId: string) => {
+  let cart = getCartFromLocalStorage();
+  if (!cart) return;
+  cart = cart.map((product) => {
+    if (product.productId === productId) {
+      product.quantity += 1;
+    }
+    return product;
+  });
+  saveCartToLocalStorage(cart)
+};
+
+export const subQuantityOfProduct = (productId: string) => {
+   let cart = getCartFromLocalStorage();
+   if (!cart) return;
+   cart = cart.map((product) => {
+     if (product.productId === productId) {
+       product.quantity -= 1;
+     }
+     return product;
+   });
+   saveCartToLocalStorage(cart)
+ };

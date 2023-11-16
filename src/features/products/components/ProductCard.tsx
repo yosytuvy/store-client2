8144174015f7d5d860170c8d8efd319b826d91fc
unknown <menashesoffer8916@gmail.com>
2023-11-16 12:18:17 +0200

    styled categorys page

--- src/features/products/components/ProductCard.tsx
+++ src/features/products/components/ProductCard.tsx
@@ -3,17 +3,28 @@ import ProductInterface from "../interfaces/productInterface";
 import { Box, Grid } from "@mui/material";
 import Item from "./Item";
 
-type ProductCardProps = { product: ProductInterface , onClick: () => void};
+type ProductCardProps = { product: ProductInterface; onClick: () => void };
 
 const ProductCard: FC<ProductCardProps> = ({ product, onClick }) => {
   return (
     <>
-      <Grid item xs={2} sm={4} md={4} key={product._id} >
-        <Item onClick={() => onClick()}>
+      <Grid item xs={2} sm={4} md={4} key={product._id}>
+        <Item
+          onClick={() => onClick()}
+          sx={{
+            margin: 3,
+            borderRadius: 5,
+            backgroundColor: "#cdf7f7",
+            "&:hover": {
+              transform: "scale(1.02)",
+              cursor: "pointer",
+            },
+          }}
+        >
           <img
             src={product.image}
             alt={product.name}
-            style={{ maxWidth: "100%", maxHeight: "150px" }}
+            style={{ maxWidth: "100%", maxHeight: "150px", borderRadius: 6 }}
           />
           <Box>{product.name}</Box>
           <Box>Company: {product.company}</Box>
--- src/features/products/pages/Category.tsx
+++ src/features/products/pages/Category.tsx
@@ -18,25 +18,41 @@ const Category = ({ title, filters }: CategoryProp) => {
     console.log("Apply Filtering clicked!");
   };
 
+  const CategoryImageURL =
+    "https://plus.unsplash.com/premium_photo-1674071397622-4ab36d7c4a27?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
   return (
     <>
-      <Box textAlign="center" marginTop={2}>
-        <Typography variant="h4" gutterBottom>
-          {title}
-        </Typography>
-        <PriceFilter />
-      </Box>
-      {filters.map((item, index) => (
-        <Filters key={index} headline={item} values={example} />
-      ))}
-
-      <Button
-        style={{ backgroundColor: "your_color_code", color: "inherit" }}
-        onClick={() => handleApplyFilterClick()}
+      <Box
+        component="div"
+        sx={{
+          position: "relative",
+          width: "100%",
+          height: "100%",
+          backgroundImage: `url(${CategoryImageURL})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
       >
-        Apply Filtering
-      </Button>
-      <Products category={category as string}/>
+        <Box textAlign="center">
+          <Typography variant="h4" gutterBottom sx={{ color: "white" }}>
+            {title}
+          </Typography>
+          <PriceFilter />
+        </Box>
+        {filters.map((item, index) => (
+          <Filters key={index} headline={item} values={example} />
+        ))}
+
+        <Button
+          style={{ backgroundColor: "your_color_code", color: "inherit" }}
+          onClick={() => handleApplyFilterClick()}
+        >
+          Apply Filtering
+        </Button>
+        <Products category={category as string} />
+      </Box>
     </>
   );
 };
